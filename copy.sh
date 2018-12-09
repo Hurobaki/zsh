14ab84936e32fda3a502ea05079ef3d2a35b356a
Theo HERVEUX <hurobaki@gmail.com>
2018-12-09 18:40:35 +0100

    add new zsh version

--- copy.sh
+++ copy.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 files="$HOME/.zshrc"
+GIT=$(which git)
+echo $GIT
 
 if [ -e $files ]; then
     $(cp $files $HOME/Saves/configs)
     if [ $? ]; then
         echo "=== $files copied ! ==="
-        $(git add . && git commit -m "add new zsh version" && git push origin master)
+        #$(git add .)git commit -m "add new zsh version" && git push origin master)
+        ${GIT} add .
+        ${GIT} commit -m 'add new zsh version'
+        ${GIT} push origin master
         echo "=== new version pushed ==="
     fi
 fi
